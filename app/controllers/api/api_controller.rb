4662e7273c7b362fcab6d1e59c0f17e43f435d55
François KLINGLER <francois@fklingler.com>
2012-06-07 10:12:44 +0200

    Add default json format for api

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,9 @@
 class Api::ApiController < ApplicationController
   self.responder = ActsAsApi::Responder
   respond_to :json
+
+  prepend_before_filter :set_default_response_format
+  def set_default_response_format
+    request.format = :json if params[:format].nil?
+  end
 end
